5b6d802107a437b42638f4c4b130d018c16da6b1
Leif <lschjeide@yahoo.com>
2014-03-09 15:00:11 +0800

    added step, now failure is missing method within location
    Failures:
    
      1) Browse Lower Taxonomies Browsing Africa a location "South Africa"
    -> the lower taxonomies should be "Cape Town, Table Mountain National
    Park"
         Failure/Error: Then the lower taxonomies should be "Cape Town,
    Table Mountain National Park"
         NoMethodError:
           undefined method `get_lower' for #<Location:0x00000103067698
    @name="South Africa">
         # spec/steps/location_steps.rb:6:in `block in <top (required)>'
         # ./spec/acceptance/browse_lower_taxonomies.feature:4:in `block (6
    levels) in run'
         # ./spec/acceptance/browse_lower_taxonomies.feature:3:in `each'
         # ./spec/acceptance/browse_lower_taxonomies.feature:3:in `block (5
    levels) in run'
         # ./spec/acceptance/browse_lower_taxonomies.feature:4:in `the
    lower taxonomies should be "Cape Town, Table Mountain National Park"'
    
    Finished in 0.00156 seconds
    1 example, 1 failure

--- spec/steps/location_steps.rb
+++ spec/steps/location_steps.rb
@@ -1,3 +1,7 @@
 step "a location :loc" do |loc|	
 	@location = Location.new(loc)
 end
+
+step "the lower taxonomies should be :locations" do |locations|
+	expect(@location.get_lower.to_s).to eq(locations)
+end
