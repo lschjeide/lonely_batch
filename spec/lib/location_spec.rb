eeadef3e85fe3a10a6fec6901c68d976f7b8ce61
Leif <lschjeide@yahoo.com>
2014-03-09 15:06:10 +0800

    Add first unit test on initializing Location

--- spec/lib/location_spec.rb
+++ spec/lib/location_spec.rb
@@ -1,6 +1,13 @@
 require "location"
 
 describe Location do
+  describe "initialization" do
+    subject(:location) { Location.new("South Africa") }
 
+    it "sets a location" do
+      expect(location.get_name).to eq("South Africa")
+    end
+
+  end
 
 end
