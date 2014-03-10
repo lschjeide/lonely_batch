b6e936672347b0a4e1d5a160d4b5ef28b117d632
Leif <lschjeide@yahoo.com>
2014-03-10 11:31:12 +0800

    Added support for file names to be passed by command line arguments
    With basic error checking

--- lonely_batch.rb
+++ lonely_batch.rb
@@ -6,8 +6,15 @@ require "./lib/location.rb"
 
 puts "executing…"
 
-batch = Batch.new("input_files/taxonomy.xml", "input_files/destinations.xml")
+#"input_files/taxonomy.xml", "input_files/destinations.xml"
 
-batch.batch_create()
+if !ARGV[1].nil? and !ARGV[0].nil? and File.exist?(ARGV[1]) and File.exist?(ARGV[0])
 
-puts "finished!"
+	batch = Batch.new(ARGV[0], ARGV[1])
+	batch.batch_create()
+	puts "finished!"
+	
+else
+	puts "Required to have file locations for taxonomy, destinations respectively."
+end
+
